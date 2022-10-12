37eb1e413ad396647777454f8683fc81401e728c
yank05 <koenigyannik@web.de>
2022-10-12 13:52:22 +0200

    TRY

---
+++ Infrastructure_Lektionen/program.js
@@ -0,0 +1,5 @@
+let v1 = [2, 6, 9];
+let v2;
+v2 = v1;
+console.log(v2);
+//# sourceMappingURL=program.js.map
---
+++ Infrastructure_Lektionen/program.ts
@@ -0,0 +1,4 @@
+let v1: number[] = [2, 6, 9]; 
+let v2: number;
+v2 = v1; 
+console.log(v2); 
