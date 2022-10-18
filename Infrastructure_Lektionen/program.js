00782e33f7c10fcddf364b692ed1b243a3fdb381
yank05 <koenigyannik@web.de>
2022-10-18 10:57:23 +0200

    ADD

--- Infrastructure_Lektionen/program.js
+++
@@ -1,5 +0,0 @@
-let v1 = [2, 6, 9];
-let v2;
-v2 = v1;
-console.log(v2);
-//# sourceMappingURL=program.js.map
--- Infrastructure_Lektionen/program.ts
+++
@@ -1,4 +0,0 @@
-let v1: number[] = [2, 6, 9]; 
-let v2: number;
-v2 = v1; 
-console.log(v2); 
