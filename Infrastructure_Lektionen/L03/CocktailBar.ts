8b7d3de6a8ed7a8123aa492a88cfaab5fb608362
yank05 <koenigyannik@web.de>
2022-10-28 10:33:34 +0200

    ADD

---
+++ Infrastructure_Lektionen/L03/CocktailBar.ts
@@ -0,0 +1,35 @@
+namespace L03_CocktailBar {
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
+        console.log("Start");
+        let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+        let slider: HTMLInputElement = <HTMLInputElement>document.querySelector("input#amount");
+        form.addEventListener("change", handleChange);
+        slider.addEventListener("input", displayAmount);
+    }
+
+    function handleChange(_event: Event): void {
+        // console.log(_event);
+        // let drink: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select"); 
+        // console.log(drink.value); 
+
+        // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input"); 
+        // console.log(inputs); 
+
+        let formData: FormData = new FormData(document.forms[0]); 
+        for (let entry of formData) {
+            let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']" ); 
+            let price: number = Number(item.getAttribute("price"));
+
+            let order: HTMLDivElement = document.querySelector("div#order"); 
+            order.innerHTML += item.name + "  €" + price; 
+        }
+    }
+
+    function displayAmount(_event: Event): void {
+        let progress: HTMLProgressElement = <HTMLProgressElement>document.querySelector("progress");
+        let amount: string = (<HTMLInputElement>_event.target).value;
+        progress.value = parseFloat(amount);
+    }
+}
