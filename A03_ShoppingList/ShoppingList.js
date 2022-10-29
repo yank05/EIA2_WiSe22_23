5fe7c914c4590b65ba95e6538e320c98ab206564
yank05 <koenigyannik@web.de>
2022-10-29 10:39:04 +0200

    ADD

--- A03_ShoppingList/ShoppingList.js
+++ A03_ShoppingList/ShoppingList.js
@@ -1,1 +1,27 @@
+var ShoppingList;
+(function (ShoppingList) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        let addButton = document.querySelector("button#add");
+        addButton.addEventListener("click", itemAdd);
+        let checkBought = document.querySelector("input#bought");
+        checkBought.addEventListener("change", itemBought);
+        let editButton = document.querySelector("button#edit");
+        editButton.addEventListener("click", editItem);
+        let deleteButton = document.querySelector("button#delete");
+        deleteButton.addEventListener("click", deleteItem);
+    }
+    function itemAdd() {
+        console.log("Item hinzugefügt");
+    }
+    function itemBought() {
+        console.log("Item als gekauft markiert");
+    }
+    function editItem() {
+        console.log("Item editieren");
+    }
+    function deleteItem() {
+        console.log("Item löschen");
+    }
+})(ShoppingList || (ShoppingList = {}));
 //# sourceMappingURL=ShoppingList.js.map
---
+++ A03_ShoppingList/ShoppingList.ts
@@ -0,0 +1,34 @@
+namespace ShoppingList {
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(): void {
+        let addButton: HTMLButtonElement = document.querySelector("button#add");
+        addButton.addEventListener("click", itemAdd); 
+
+        let checkBought: HTMLElement = document.querySelector("input#bought");
+        checkBought.addEventListener("change", itemBought); 
+
+
+        let editButton: HTMLButtonElement = document.querySelector("button#edit"); 
+        editButton.addEventListener("click", editItem);
+
+        let deleteButton: HTMLButtonElement = document.querySelector("button#delete");
+        deleteButton.addEventListener("click", deleteItem); 
+    }
+
+    function itemAdd(): void {
+        console.log("Item hinzugefügt");
+    }
+
+    function itemBought(): void {
+        console.log("Item als gekauft markiert");
+    }
+
+    function editItem(): void {
+        console.log("Item editieren");
+    }
+
+    function deleteItem(): void {
+        console.log("Item löschen"); 
+    }
+}
